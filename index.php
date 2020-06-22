40026fe13e48b0155a6f84d7f213c577ea90e460
Anindya CISPL <anindya.bhattacharya@codeclouds.in>
2020-06-22 11:57:14 +0530

    Episode 15
    Before starting Router

--- index.php
+++ index.php
@@ -1,32 +1,7 @@
 <?php
+$query = require 'bootstrap.php';
+require 'Task.php';
 
-class Task
-{
-    public $description;
-    public $completed = false;
-    public function __construct($description)
-    {
-        $this->description = $description;
-    }
-
-    public function isComplete()
-    {
-        return $this->completed;
-    }
-
-    public function complete()
-    {
-        $this->completed = true;
-    }
-}
-
-
-$tasks = [
-    new Task('Complete office work'),
-    new Task('Start your project'),
-    new Task('Make your mind empty'),
-];
-
-$tasks[0]->complete();
+$tasks = $query->selectAll('todos', 'Task');
 
 require 'index.view.php';
