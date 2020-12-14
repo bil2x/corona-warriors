3bdc352fd3f60a35eca78027421ebfb0b7a59490
bil2x <bil2x33@gmail.com>
2020-12-14 14:08:17 +0530

    Phase 1 of Image Upload
    Multiple image can be added and display.
    Disabling the edit of multi images as it's seem very costly
    Need to use Javascript to add that propery

--- app/views/stories/show.view.php
+++ app/views/stories/show.view.php
@@ -12,6 +12,7 @@
             <h3><?= $story[0]->name; ?></h3>
             <p><?= date('F j,Y', strtotime($story[0]->created_at)); ?> <?= $story[0]->district; ?>, <?= $story[0]->state; ?></p>
             <h2><?= $story[0]->title; ?></h2>
+            <iframe width="700" height="315" src="https://www.youtube.com/embed/<?= substr(strstr($story[0]->video, '?v='), 3); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
             <img src="<?= ltrim(getimage('story_id', $story[0]->id, 1)[0]->location ??= './public/images/default.webp', '.'); ?>" alt="" width="700" height="450">
             <p>
                 <?php
@@ -19,7 +20,7 @@
                 //  dd($allImages);
                 foreach ($allImages as $image) {
                 ?>
-                    <img src="<?= ltrim($image->location, '.'); ?>" alt="100" width="75">
+                    <a href="<?= ltrim($image->location, '.'); ?>" target="_blank"><img src="<?= ltrim($image->location, '.'); ?>" alt="100" width="75"></a>
                 <?php } ?>
             </p>
             <?php
