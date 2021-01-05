15d036615969fd8e8bd172600360d1da8ae08f6a
Anindya CISPL <anindya.bhattacharya@codeclouds.in>
2021-01-05 22:27:58 +0530

    Zero Javascript Modal Added

--- app/views/admin/images.view.php
+++ app/views/admin/images.view.php
@@ -9,10 +9,16 @@ require('includes/header.view.php');
         foreach ($images as $image) :
         ?>
 
-            <p>
-                <a href="<?= ltrim($image->location, '.'); ?>" target="_blank"><img src="<?= ltrim($image->location, '.'); ?>" height="200" width="175"></a>
-                <a href="/admin/images/delete?id=<?= $image->id; ?>"> <span><i class="fas fa-trash"></i></span></a>
-            </p>
+            <div class="image">
+                <a href="#<?= ltrim($image->location, '.'); ?>">
+                    <img src="<?= ltrim($image->location, '.'); ?>" height="200" width="175">
+                    <a class="trash" href="/admin/images/delete?id=<?= $image->id; ?>" title="Delete Image"> <span><i class="fas fa-times-circle"></i></span></a>
+                </a>
+            </div>
+            <div id="<?= ltrim($image->location, '.'); ?>" class="overlay">
+                <a href="#" class="cancel"></a>
+                <div class="modal"><img src="<?= ltrim($image->location, '.'); ?>" alt="" width="700" height="450"></div>
+            </div>
         <?php endforeach; ?>
     </div>
 
--- app/views/stories/show.view.php
+++ app/views/stories/show.view.php
@@ -13,16 +13,20 @@
             <p><?= date('F j,Y', strtotime($story[0]->created_at)); ?> <?= $story[0]->district; ?>, <?= $story[0]->state; ?></p>
             <h2><?= $story[0]->title; ?></h2>
             <iframe width="700" height="315" src="https://www.youtube.com/embed/<?= substr(strstr($story[0]->video, '?v='), 3); ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            <img src="<?= ltrim(getimage('story_id', $story[0]->id, 1)[0]->location ??= './public/images/default.webp', '.'); ?>" alt="" width="700" height="450">
-            <p>
-                <?php
-                $allImages = getimage('story_id', $story[0]->id);
-                //  dd($allImages);
-                foreach ($allImages as $image) {
-                ?>
-                    <a href="<?= ltrim($image->location, '.'); ?>" target="_blank"><img src="<?= ltrim($image->location, '.'); ?>" alt="100" width="75"></a>
-                <?php } ?>
-            </p>
+            <img src="<?= ltrim(getimage('story_id', $story[0]->id, 1)[0]->location ??= './public/images/default.webp', '.'); ?>" alt="" width="700" height="450" id="primary">
+
+            <?php
+            $allImages = getimage('story_id', $story[0]->id);
+            //  dd($allImages);
+            foreach ($allImages as $image) {
+            ?>
+                <a href="#<?= ltrim($image->location, '.'); ?>"><img src="<?= ltrim($image->location, '.'); ?>" alt="100" width="75"></a>
+                <div id="<?= ltrim($image->location, '.'); ?>" class="overlay">
+                    <a href="#primary" class="cancel"></a>
+                    <div class="modal"><img src="<?= ltrim($image->location, '.'); ?>" alt="" width="700" height="450"></div>
+                </div>
+
+            <?php } ?>
             <?php
             if (auth()) {
                 if (auth()->id == $story[0]->user_id) {
